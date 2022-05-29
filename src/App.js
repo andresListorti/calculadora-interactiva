f3b7797c0fb2b5a6b89e24176e01718c4b9f1f6a
andresListorti <andreslistorti@gmail.com>
2022-05-29 19:52:33 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import freeCodelogo from './imagenes/fcclogo.jpg'
 
 function App() {
   return (
-    <div className="App">
-      logoestare
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        src={freeCodelogo}
+        className='freecodecamp-logo'
+        alt='logo de freecodecamp' />
+      </div>
+    <div className='contenedor-calculadora'>
+
+    </div>
+      
     </div>
   );
 }
