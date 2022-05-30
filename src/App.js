3822d31d1a7b2f3ff784bd412d4563155249d514
andresListorti <andreslistorti@gmail.com>
2022-05-30 19:02:37 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import freeCodelogo from './imagenes/fcclogo.jpg'
+import freeCodelogo from './imagenes/fcclogo.jpg';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
@@ -11,7 +12,14 @@ function App() {
         alt='logo de freecodecamp' />
       </div>
     <div className='contenedor-calculadora'>
-
+      <div className='fila'>
+        <Boton>1</Boton> 
+        <Boton>+</Boton> 
+      </div>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>
     </div>
       
     </div>
